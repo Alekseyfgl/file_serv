ebf83817f8bb29ea00895750dec03bfa8880d2b2
aleksey.fgl <aleksey.fgl@gmail.com>
2025-02-23 14:01:52 +0000

    fix cors

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
+	"time"
 )
 
 func main() {
@@ -20,10 +21,24 @@ func main() {
 	// Инициализируем новый роутер (без встроенных логов)
 	r := gin.New()
 
-	// Добавляем CORS middleware, разрешающий запросы с любых источников
-	r.Use(cors.Default())
+	// Настройка CORS для разрешения всех запросов и поддержки отправки файлов
+	corsConfig := cors.Config{
+		// Разрешаем запросы с любых источников
+		AllowAllOrigins: true,
+		// Разрешаем все основные HTTP-методы
+		AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		// Разрешаем основные заголовки, включая необходимые для отправки файлов
+		AllowHeaders: []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		// Заголовки, которые могут быть видны на стороне клиента
+		ExposeHeaders: []string{"Content-Length"},
+		// Если требуется, можно передавать куки
+		AllowCredentials: true,
+		// Время, в течение которого результаты preflight-запроса кэшируются
+		MaxAge: 12 * time.Hour,
+	}
+	r.Use(cors.New(corsConfig))
 
-	// Ограничение, что при multipart-файлах > 8 MB данные пойдут во временный файл
+	// Ограничение: при multipart-файлах > 8 MB данные пойдут во временный файл
 	r.MaxMultipartMemory = 8 << 20
 
 	// Добавляем остальные middleware
@@ -36,7 +51,7 @@ func main() {
 
 	routes.S3Routes(apiGroup, container.S3Handler)
 
-	port := env.GetEnv("SERV_PORT", "8080")
+	port := env.GetEnv("SERV_PORT", "3000")
 	log.Info("Starting server", zap.String("port", port))
 	if err := r.Run(":" + port); err != nil {
 		log.Fatal("Failed to start server", zap.Error(err))
