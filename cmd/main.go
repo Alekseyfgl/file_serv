db3d1adc017c432a580bd15b5aa6a70c62b9b7f9
aleksey.fgl <aleksey.fgl@gmail.com>
2025-02-23 13:54:32 +0000

    add cors

--- cmd/main.go
+++ cmd/main.go
@@ -6,13 +6,12 @@ import (
 	"files/internal/ioc"
 	"files/internal/routes"
 	"files/pkg/log"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 )
 
 func main() {
-	initApp()
-
 	container := ioc.NewContainer()
 
 	// Отключаем режим отладки, чтобы не выводились лишние сообщения
@@ -20,12 +19,18 @@ func main() {
 
 	// Инициализируем новый роутер (без встроенных логов)
 	r := gin.New()
-	apiGroup := r.Group("/files")
+
+	// Добавляем CORS middleware, разрешающий запросы с любых источников
+	r.Use(cors.Default())
+
 	// Ограничение, что при multipart-файлах > 8 MB данные пойдут во временный файл
 	r.MaxMultipartMemory = 8 << 20
+
+	// Добавляем остальные middleware
 	r.Use(gin.Recovery())
 	r.Use(middlewares.RequestLoggerMiddleware(container.Logger))
 
+	apiGroup := r.Group("/files")
 	//set auth for group
 	//apiGroup.Use(auth.JwtAuthMiddleware(container.JwtService))
 
@@ -36,8 +41,4 @@ func main() {
 	if err := r.Run(":" + port); err != nil {
 		log.Fatal("Failed to start server", zap.Error(err))
 	}
-}
-
-func initApp() {
-
 }
